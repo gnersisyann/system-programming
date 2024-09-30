077e23a56c8a144f40d4d83b2b7d66f24f7bef9c
Garnik <garnik.nersisyan@student.rau.am>
2024-09-30 21:27:10 +0400

    modified FS-1, added FS-2, FS-3

--- FS-1/main.cpp
+++ FS-1/main.cpp
@@ -9,18 +9,30 @@ int main (int argc, char **argv)
 	{
 		int fd = open(argv[1], O_RDONLY);
 		if (fd == -1)
+		{
 			std::cout<<"Error opening file, code:"<<errno<<std::endl;
+			return errno;
+		}
 		const int BUFFER_SIZE = 256;
 		char buf[BUFFER_SIZE];
 		int bytes_read;
 		while((bytes_read = read(fd, buf, BUFFER_SIZE)) > 0)
 			std::cout.write(buf, bytes_read);
 		if (bytes_read == -1)
+		{
 			std::cout<<"Error reading file, code:"<<errno<<std::endl;
+			return errno;
+		}
 		if (close(fd) == -1)
-			std::cout<<"ERror closing file, code:"<<errno<<std::endl;
+		{
+			std::cout<<"Error closing file, code:"<<errno<<std::endl;
+			return errno;
+		}
 	}
 	else
+	{
 		std::cout<<"Invalid arguments"<<std::endl;
-
+		return 1;
+	}
+	return 0;
 }
---
+++ FS-2/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fcntl.h>
+#include <unistd.h>
+#include <cerrno>
+
+int main (int argc, char **argv)
+{
+	if (argc == 3)
+	{
+		char BUF[256];
+		int sfd = open(argv[1], O_RDONLY);
+		if (sfd == -1)
+		{
+			std::cout<<"Error opening source file, code "<<errno<<std::endl;
+			return errno;
+		}
+		
+		int dfd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+		if (dfd == -1)
+		{
+			close (sfd);
+			std::cout<<"Error opening destination file, code "<<errno<<std::endl;
+			return errno;
+		}
+		int bytes_read;
+		int bytes_written;
+		while ((bytes_read = read(sfd, BUF, sizeof(BUF))) > 0)
+		{
+			bytes_written = write(dfd, BUF, bytes_read);
+		}
+		if (bytes_written == -1)
+		{
+			close (sfd);
+			close (dfd);
+			std::cout<<"Error writing file, code "<<errno<<std::endl;
+			return errno;
+		}
+		if (bytes_read == -1)
+		{
+			
+			close (sfd);
+			close (dfd);
+			std::cout<<"Error reading file, code "<<errno<<std::endl;
+			return errno;
+		}
+		close (sfd);
+		close (dfd);
+		std::cout<<"File was created successfully"<<std::endl;
+		return 0;
+	}
+	else
+	{
+		std::cout<<"Invalid arguments, use [source file] [destination file]"<<std::endl;
+		return 1;
+	}
+}
