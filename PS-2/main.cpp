647a75325bedc83e07c239adc468b51d1a5279df
Garnik <garnik.nersisyan@student.rau.am>
2024-10-29 20:29:56 +0400

    updated PS-2

--- PS-2/main.cpp
+++ PS-2/main.cpp
@@ -7,84 +7,123 @@
 #include <unistd.h>
 
 void split_input(char *input, char **args) {
-  char *token = strtok(input, " ");
-  int index = 0;
-  while (token != nullptr) {
-    args[index++] = token;
-    token = strtok(nullptr, " ");
-  }
-  args[index] = nullptr;
+    char *token = strtok(input, " ");
+    int index = 0;
+    while (token != nullptr) {
+        args[index++] = token;
+        token = strtok(nullptr, " ");
+    }
+    args[index] = nullptr;
 }
 
-void do_command(char *input) {
-  char *argv[256];
-  char *redirect = strchr(input, '>');
-  bool append = false;
-  if (redirect != nullptr) {
-    *redirect = '\0';
-    redirect++;
-    if (*redirect == '>') {
-      append = true;
-      *redirect = '\0';
-      redirect++;
-    }
-    while (*redirect == ' ' || *redirect == '\t')
-      redirect++;
-  }
-  pid_t pid = fork();
-  if (pid < 0) {
-    std::cout << "Fork failed" << std::endl;
-    return;
-  } else if (pid == 0) {
-    if (redirect) {
-      int fd;
-      if (append)
-        fd = open(redirect, O_WRONLY | O_CREAT | O_APPEND, 0644);
-      else
-        fd = open(redirect, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-      if (fd < 0) {
-        std::cerr << "Error opening/creating file" << std::endl;
-        exit(1);
-      }
-      dup2(fd, 1);
-      close(fd);
-    }
-    if (strncmp(input, "silent", 6) == 0) {
-      int child_pid = getpid();
-      char pid_str[20];
-      snprintf(pid_str, 20, "%d.log", child_pid);
-      int fd = open(pid_str, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-      if (fd < 0) {
-        std::cerr << "Error opening/creating PID file" << std::endl;
-        exit(1);
-      }
-      dup2(fd, 1);
-      dup2(fd, 2);
-      close(fd);
+bool is_exit_command(char *command) {
+    char *tmp = command;
+    while (*tmp == ' ') tmp++;
+    return (strncmp(tmp, "exit", 4) == 0);
+}
 
-      split_input(input + 7, argv);
-      execvp(argv[0], argv);
-      std::cerr << "Execution failed" << std::endl;
-      exit(1);
+int execute_command(char *command) {
+    if (is_exit_command(command))
+        return 1;
 
+    char *argv[256];
+    char *redirect = strchr(command, '>');
+    bool append = false;
+
+    if (redirect != nullptr) {
+        *redirect = '\0';
+        redirect++;
+        if (*redirect == '>') {
+            append = true;
+            *redirect = '\0';
+            redirect++;
+        }
+        while (*redirect == ' ' || *redirect == '\t')
+            redirect++;
+    }
+
+    pid_t pid = fork();
+    if (pid < 0) {
+        std::cout << "Fork failed" << std::endl;
+        return -1;
+    } else if (pid == 0) {
+        if (redirect) {
+            int fd;
+            if (append)
+                fd = open(redirect, O_WRONLY | O_CREAT | O_APPEND, 0644);
+            else
+                fd = open(redirect, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (fd < 0) {
+                std::cerr << "Error opening/creating file" << std::endl;
+                exit(1);
+            }
+            dup2(fd, 1);
+            close(fd);
+        }
+
+        split_input(command, argv);
+        execvp(argv[0], argv);
+        std::cerr << argv[0] << ": command not found" << std::endl;
+        exit(1);
     } else {
-      split_input(input, argv);
-      execvp(argv[0], argv);
-      std::cerr << "Execution failed" << std::endl;
-      exit(1);
+        int status;
+        wait(&status);
+        return WIFEXITED(status) ? WEXITSTATUS(status) : -1;
     }
-  } else {
-    wait(NULL);
-  }
+}
+
+int process_command(char *command) {
+    char *and_pos = nullptr;
+    char *or_pos = nullptr;
+    int result;
+
+    while (true) {
+        and_pos = strstr(command, "&&");
+        or_pos = strstr(command, "||");
+
+        if (and_pos != nullptr) {
+            *and_pos = '\0';
+            result = execute_command(command);
+            if (result == 1) return 1;
+            command = and_pos + 2;
+        } else if (or_pos != nullptr) {
+            *or_pos = '\0';
+            result = execute_command(command);
+            if (result != 0) break;
+            command = or_pos + 2;
+        } else {
+            result = execute_command(command);
+            if (result == 1) return 1;
+            break;
+        }
+    }
+    return 0;
+}
+
+int do_command(char *input) {
+    char *command = strtok(input, ";");
+    while (command != nullptr) {
+        while (*command == ' ') command++;
+
+        if (is_exit_command(command))
+            return 1;
+
+        if (process_command(command))
+            return 1;
+
+        command = strtok(nullptr, ";");
+    }
+    return 0;
 }
 
 int main() {
-  char input[256];
-  while (true) {
-    std::cout << ">: ";
-    std::cin.getline(input, 256);
-    if (strcmp(input, "exit") == 0)
-      break;
-    do_command(input);
-  }
+    char input[256];
+    while (true) {
+        std::cout << ">: ";
+        std::cin.getline(input, 256);
+        if (do_command(input) == 1)
+            break;
+    }
+    return 0;
 }
+
